eab7f6218630fc5461faabf845a4ee4ea278604e
Lemon Liu <47017764+lemon9527@users.noreply.github.com>
2019-06-02 23:35:56 +0800

    Update 02.c

--- ch03/02.c
+++ ch03/02.c
@@ -1,1 +1,16 @@
-//test
+//Chapter 3 Exercise 2:
+// Write a program asks you enter an ASCII code value, such as 66, and
+// then prints the character having that ASCII value.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int ASCII_code;
+    printf("Please Enter an ASCII code (from 0~127): \n");
+    scanf("%d", &ASCII_code);
+
+    printf("Character for %d is %c\n", ASCII_code, ASCII_code);
+
+    return 0;
+}
