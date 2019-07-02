4060687e55822d91bc82469de8979f61b3b06277
Lemon Liu <47017764+lemon9527@users.noreply.github.com>
2019-07-02 23:53:20 +0800

    Exercise 03

--- ch10/03.c
+++ ch10/03.c
@@ -8,24 +8,34 @@ int max_in_arr(int * arr, int arr_size);
 
 int main(void)
 {
-    int arr[SIZE] = {1, 2, 3, 41, 5, 6, 7, 8, 10, 9};
+    int arr[SIZE] = {43, 2, 3, 41, 5, 6, 7, 8, 10, 9};
 
     printf("The max value in arr[10] is %d\n", max_in_arr(arr, SIZE));
 
     return 0;
 }
 
+// int max_in_arr(int * arr, int arr_size)
+// {
+//     for(int i = 0; i < arr_size; i++)
+//     {
+//         if(arr[i]>=arr[i+1] && (i+1)<arr_size)
+//         {
+//             int temp;
+//             temp = arr[i];
+//             arr[i] = arr[i+1];
+//             arr[i+1] = temp;
+//         }
+//     }
+//     return arr[arr_size-1];
+// }
 int max_in_arr(int * arr, int arr_size)
 {
-    for(int i = 0; i < arr_size; i++)
+	int max = arr[0];
+    for(int i = 1; i < arr_size; i++)
     {
-        if(arr[i]>=arr[i+1] && (i+1)<arr_size)
-        {
-            int temp;
-            temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
-        }
+        if(arr[i] > max)
+        	max = arr[i];
     }
-    return arr[arr_size-1];
+    return max;
 }
