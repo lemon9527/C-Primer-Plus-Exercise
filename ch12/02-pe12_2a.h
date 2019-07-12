0941cf85246fe919f63d589e3e042d2046049833
Lemon Liu <47017764+lemon9527@users.noreply.github.com>
2019-07-12 13:40:07 +0800

    Create 02-pe12_2a.h

---
+++ ch12/02-pe12_2a.h
@@ -0,0 +1,13 @@
+#ifndef PE12_2A_H_INCLUDED
+#define PE12_2A_H_INCLUDED
+
+extern int mode;
+extern float distance;
+extern float fuel_consumed;
+
+
+extern void set_mode(int mode);
+extern void get_info(void);
+extern void show_info(void);
+
+#endif // PE12_2A_H_INCLUDED
