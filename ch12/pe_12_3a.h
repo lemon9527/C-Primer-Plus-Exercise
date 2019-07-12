667f522d25fff9f3ab70139feba85aeb4c971d49
Lemon Liu <47017764+lemon9527@users.noreply.github.com>
2019-07-12 14:47:51 +0800

    Create pe_12_3a.h

---
+++ ch12/pe_12_3a.h
@@ -0,0 +1,8 @@
+#ifndef PE12_3A_H_INCLUDED
+#define PE12_3A_H_INCLUDED
+
+void set_mode(int * mode);
+void get_info(int mode, double *distance, double *fuel_consumed);
+void show_info(int mode, double distance, double fuel_consumed);
+
+#endif // PE12_3A_H_INCLUDED
