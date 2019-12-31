aee0dd340b4a29506f7d8ab2d26c4c3227370ff3
Lemon Liu <47017764+lemon9527@users.noreply.github.com>
2019-12-31 13:50:41 +0800

    Update 03.c

--- ch14/03.c
+++ ch14/03.c
@@ -1,94 +1,86 @@
-/*********************************Chapter 14 Exercise***************************/
-//#03
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
+//03.c 
+//修改程序14.2中的图书目录程序，使其按照输入图书的顺序输出图书的信息，
+//然后按照标题字母的顺序输出图书信息，最后按照价格升序输出图书的信息。
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
 
 #define MAXTITL 40
 #define MAXAUTL 40
 #define MAXBKS  100
 
-struct book {       //建立book 结构模板
+struct book{
     char title[MAXTITL];
     char author[MAXAUTL];
     float value;
 };
 
-char *s_gets(char * st, int n);
-void titlesort(struct book * books[], int count);
-//void valuesort(struct book *, int count);
+char *s_gets(char *st, int n);
+void title_sort(struct book *[], int count);
+void value_sort(struct book *[], int count);
 
 int main(void)
 {
-    struct book * library[MAXBKS];    //声明book结构模板类型的指针数组
-    //结构体指针其实是一个指针，它存储的是指向某个结构体的“地址”，
-    //所以结构体指针数组存储的其实是指向一组结构体的“地址”数组，
-    //它本身并不包含结构体的具体内容（没有被分配内存）
-    /****************************/
-    //所以当用到他们时，要么重定向，比如：
-	//d=&b; //让d指向b，d存储的是b的地址，这样就能通过d来调用b了
-	//要么用malloc为他们分配内存：
-	//d=(struct h *)malloc(sizeof(struct h));
-	//这样d就指向系统为其分配的一块内存，其内容就是struct h中的东西
+    struct book * library[MAXBKS];//声明book结构类型的指针数组, 需要用malloc分配内存
 
     char temp[MAXTITL];
     int count = 0;
     int index;
 
     printf("Please enter the book title.\n");
-    printf("Press [enter] at the start of a line to stop.\n");
+    printf("Press [enter] at the start of a line to quit.\n");
     while(count < MAXBKS && s_gets(temp, MAXTITL) != NULL
-          && temp[0] != '\0')
+        && temp[0] != '\0')
     {
-    	//结构体指针数组存储的其实是指向一组结构体的“地址”数组， 没有分配内存，需要重定向
-    	//library[count]， count =0 时，即时第一个结构指针
-    	library[count] = (struct book *) malloc(sizeof (struct book));
-    	strncpy(library[count]->title, temp, MAXTITL);
-
         printf("Now enter the author.\n");
+        //library[count], count = 0 时，即第一个结构指针
+        library[count] = (struct book *)malloc(sizeof(struct book));
+        strcpy(library[count]->title, temp);
         s_gets(library[count]->author, MAXAUTL);
-
         printf("Now enter the value.\n");
-        scanf("%f", &library[count]->value);    //获取输入的value后，递增count
-        while(getchar()!='\n')
-            continue;       //清理输入行
-        if(count < MAXTITL)
-            printf("Enter the next title.\n");
-
+        scanf("%f", &library[count]->value);
         count++;
+        while(getchar()!='\n')
+            continue;       //clear input stream
+        if(count < MAXBKS)
+            printf("Enter the next title.\n");
     }
 
-    //output list of books
     if(count > 0)
     {
-        //by input order
-        printf("Here is the list of your books (in the order of input):\n");
+        //print books by input sequence
+        printf("Here is the list of your books by input sequence:\n");
         for(index = 0; index < count; index++)
-            printf("%s by %s : $%.2f\n", library[index]->title, library[index]->author,
-                   library[index]->value);
-
-        //sorted by title
-        printf("Here is the list of your books (in the order of title):\n");
-        titlesort(library, count);
+            printf("%s by %s: $%.2f\n", library[index]->title, 
+                library[index]->author, library[index]->value);
+        
+        //print books by title
+        printf("Here is the list of your books by 1st letter of title:\n");
+        title_sort(library, count);
         for(index = 0; index < count; index++)
-            printf("%s by %s : $%.2f\n", library[index]->title, library[index]->author,
-                   library[index]->value);
+            printf("%s by %s: $%.2f\n", library[index]->title, 
+                library[index]->author, library[index]->value);
 
+        //print books by value
+        printf("Here is the list of your books by value:\n");
+        value_sort(library, count);
+        for(index = 0; index < count; index++)
+            printf("%s by %s: $%.2f\n", library[index]->title, 
+                library[index]->author, library[index]->value);
     }
     else
-        printf("No books? Too bad.\n");
-
-    return 0;
+        printf("No books? Too bad.\n");    
 }
 
-void titlesort(struct book * books[], int count)	//也可以写作void titlesort(struct book ** books, int count)
+void title_sort(struct book * books[], int count)
 {
+    int i, j;
     struct book * temp;
-    for(int i = 0; i < count; i++)
+    for(i = 0; i < count; i++)
     {
-        for(int j = i+1; j < count; j++)
-        {
+        for(j = i+1; j < count; j++)
+        {   //两个title 首字母比较，较大的说明顺序靠后，
             if(tolower(books[i]->title[0]) > tolower(books[j]->title[0]))
             {
                 temp = books[i];
@@ -99,21 +91,38 @@ void titlesort(struct book * books[], int count)	//也可以写作void titlesort
     }
 }
 
+void value_sort(struct book * books[], int count)
+{
+    int i, j;
+    struct book * temp;
+    for(i = 0; i < count; i++)
+    {
+        for(j = i+1; j < count; j++)
+        {   //两个value 比较，books[i] 与value小的交换
+            if(tolower(books[i]->value) > tolower(books[j]->value))
+            {
+                temp = books[i];
+                books[i] = books[j];
+                books[j] = temp;
+            }
+        }
+    }
+}
 
 char * s_gets(char *st, int n)
 {
-    char *ret_val;
-    char *find;
+   char *ret_val;
+   char *find;
 
-    ret_val = fgets(st, n, stdin);
-    if(ret_val)
-    {
-        find = strchr(st, '\n');
-        if(find)
-            *find = '\0';
-        else
-            while(getchar()!='\n')
-                continue;
-    }
-    return ret_val;
+   ret_val = fgets(st, n, stdin);
+   if(ret_val)
+   {
+       find = strchr(st, '\n');
+       if(find)
+           *find = '\0';
+       else
+           while(getchar()!='\n')
+               continue;
+   }
+   return ret_val;
 }
