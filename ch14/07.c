1b5bd3f35a0e59a1cbda17f094e3af71c90a1468
Lemon Liu <47017764+lemon9527@users.noreply.github.com>
2020-01-03 10:56:24 +0800

    Update 07.c

--- ch14/07.c
+++ ch14/07.c
@@ -2,108 +2,124 @@
 //#07 修改14.14 booksave.c, 允许用户删除记录或修改记录
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
+
 #define MAXTITL 40
 #define MAXAUTL 40
-#define MAXBKS  10  //最大书籍数量
-
-char * s_gets(char *st, int n);
+#define MAXBKS  10
 
 struct book{
-   char title[MAXTITL];
-   char author[MAXAUTL];
-   float value;
+    char title[MAXTITL];
+    char author[MAXAUTL];
+    float value;
 };
 
-void editbook(struct book *);
+char *s_gets(char * st, int n);
+void modify(struct book *);
 
 int main(void)
 {
-   struct book library[MAXBKS];    //结构数组
-   int count = 0;
-   int index, filecount, ch;
-   FILE * pbooks;
-   int size = sizeof(struct book);
+    struct book library[MAXBKS];    //结构数组
+    int count = 0;
+    int index, filecount, ch;
+    FILE * pbooks;
+    int size = sizeof(struct book);
 
-   if((pbooks = fopen("book.dat", "a+b")) == NULL)
-   {
-       fputs("Can't open book.dat file\n", stderr);
-       exit(1);
-   }
+    if((pbooks = fopen("book.dat", "rb")) == NULL)
+    {
+        fputs("Can't open book.dat file\n", stderr);
+        exit(1);
+    }
 
-   rewind(pbooks); //定位到文件开头
-   //如下while循环每次把一个结构读取到结构数组中， 档数组满或者读完文件时停止
-   while (count < MAXBKS &&fread(&library[count],size, 1, pbooks) == 1)
-   {
-       if(count == 0)
-           puts("Current contents of book.dat:");
-       printf("%s by %s: $%.2f\n", library[count].title,
-              library[count].author, library[count].value);
+    rewind(pbooks); //定位到文件开始处
+    while(count < MAXBKS && fread(&library[count], size, 1, pbooks) == 1)
+    {
+        if (count == 0)
+            puts("Current contents of book.dat:");
+        printf("%s by %s: $%.2f\n", library[count].title, library[count].author,
+            library[count].value);
+        printf("Please select an action:\n");
+        printf("n) next\n");
+        printf("d) delete\n");
+        printf("m) modify\n");
 
-       printf("Please select an action: \n"
-       			"[n] next record [d] delete this record "
-       			"[m] modify this record.\n");
-       ch=getchar();
-       while(getchar()!='\n') continue;
-       switch(ch)
-       {
-       		case ('n'):
-       		case ('N'):
-       			break;
-       		case ('m'):
-       		case ('M'):
-       			editbook(&library[count]);	//按结构地址传递结构
-       			break;
-       		case ('d'):
-       		case ('D'):
-       			count--;
-       			break;
-       		default:
-       			break;
-       }
+        ch = tolower(getchar());
+        while(getchar()!='\n') continue;
+        switch (ch)
+        {
+            case ('n'):
+                break;
+            case ('m'):
+                modify(&library[count]);   //按结构地址传递结构
+                break;
+            case ('d'):
+                count--;
+                break;
+            default:
+                break;
+        }
+        count++;
+    }
 
-       count++;
-   }
-   filecount = count;
-   if(count == MAXBKS)
-   {
-       fputs("The book.dat file is full.", stderr);
-       exit(2);
-   }
+    fclose(pbooks);
 
-   puts("Please add new book titles.");
-   puts("Press [enter] at the start of a line to stop.");
-   //这个while循环提示并接受用户的输入， 当数组已满或者用户在第一行开始按下enter时，循环结束
-   while(count < MAXBKS && s_gets(library[count].title, MAXTITL) != NULL
-         && library[count].title[0] != '\0')
-   {
-       puts("Now enter the author.");
-       s_gets(library[count].author, MAXAUTL);
-       puts("Now enter the value.");
-       scanf("%f", &library[count++].value);
-       while(getchar()!='\n')
-           continue;   //清理输入行
-       if(count < MAXBKS)
-           puts("Enter the next title.");
-   }
+    if(count==MAXBKS)
+        printf("The file 'book.dat' is full.\n");
+    else
+    {
+        puts("Please add new book title.");
+        puts("Press [enter] at the start of a line to stop.");
+        while(count < MAXBKS && s_gets(library[count].title, MAXTITL) != NULL
+            && library[count].title[0] != '\0')
+        {
+            puts("Now enter the author.");
+            s_gets(library[count].author, MAXAUTL);
+            puts("Now enter the value.");
+            scanf("%f", &library[count++].value);
+            while(getchar()!='\n')
+                continue;           //清理输入行
+            if(count < MAXBKS)
+                puts("Enter the next title.");
+        }
+    }
 
-   if(count > 0)
-   {
-       puts("Here is the list of your books:");
-       for(index = 0; index < count; index++)
-           printf("%s by %s: $%.2f\n", library[index].title,
-                  library[index].author, library[index].value);
+    if((pbooks = fopen("book.dat", "wb")) == NULL)
+    {
+        fprintf(stderr, "Could not open file 'book.dat' for write.\n");
+        exit(EXIT_FAILURE);
+    }
 
-       //打印到文件
-       fwrite(&library[filecount], size, count - filecount, pbooks);
-   }
-   else
-       puts("No books? Too bad.\n");
+    if(count>0)
+    {
+        puts("Here is the list of your books:");
+        for(index = 0; index < count; index++)
+        {
+            printf("%s by %s: $%.2f\n", library[index].title,
+                library[index].author, library[index].value);
+        }
+        fwrite(library, size, count, pbooks);
+    }
+    else
+        puts("No books? Too bad.\n");
+    puts("Bye.\n");
+    fclose(pbooks);
 
-   puts("Bye.\n");
-   fclose(pbooks);
+    return 0;
+}
 
-   return 0;
+void modify(struct book * PBK)
+{
+    puts("Please enter the new title for this book.");
+    s_gets(PBK->title, MAXTITL);
+    puts("Now enter the new author for this book.");
+    s_gets(PBK->author, MAXAUTL);
+    puts("Now enter the new value for this book.");
+    scanf("%f", &PBK->value);    //这里 PBK->value 即相当于 library[count].value, 是一个float变量，前面要加&
+    while(getchar()!='\n')
+        continue;
+
 }
 
 char * s_gets(char *st, int n)
@@ -122,16 +138,4 @@ char * s_gets(char *st, int n)
                continue;
    }
    return ret_val;
-}
-
-void editbook(struct book * BK)
-{
-	puts("Please enter new title for this book:");
-	s_gets(BK->title, MAXTITL);
-	puts("Please enter new author for this book:");
-	s_gets(BK->author, MAXAUTL);
-	puts("Please enter new value for this book:");
-	scanf("%f", &BK->value);
-
-	while(getchar()!='\n') continue;
 }
